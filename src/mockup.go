3199c7c84a91dd3a4947528bd4f1eac5b7f88dc9
AshGale <ashgale17@yahoo.co.uk>
2021-10-31 13:30:43 +0000

    started to figure out the low of tiles for a row

--- src/mockup.go
+++ src/mockup.go
@@ -1,10 +1,13 @@
 package src
 
 import (
+	"container/list"
 	"fmt"
 	"image/color"
 	"log"
 	"net/url"
+
+	"time"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
@@ -38,19 +41,6 @@ func Mockup() {
 	myWindow.ShowAndRun()
 }
 
-func getTabContent() *fyne.Container {
-
-	//maybe replace with static text to prevent scolling
-	left := getDatesTable()
-	topTimes := getTimesTable(7, 13)
-	topSpacer := getTopSpacer(left.Size().Width, topTimes.Size().Height)
-	top := container.New(layout.NewBorderLayout(nil, nil, topSpacer, nil), topSpacer, topTimes)
-
-	board := getBoard()
-
-	return container.New(layout.NewBorderLayout(top, nil, left, nil), top, left, board)
-}
-
 func getTopSpacer(width, hight float32) fyne.CanvasObject {
 	// size := fyne.NewSize(width, hight)
 	// position := fyne.NewPos(0,0)
@@ -60,33 +50,29 @@ func getTopSpacer(width, hight float32) fyne.CanvasObject {
 	return spacer
 }
 
-func getTile(index int) *fyne.Container {
+func getTile(taskTile, urlString string, duration int) *fyne.Container {
 	tile := container.New(layout.NewVBoxLayout())
 	size := fyne.NewSize(200, 100)
 
-	url, _ := url.Parse("https://developer.fyne.io/api/v2.1/widget/hyperlink.html")
-	hyperlink := widget.NewHyperlink(fmt.Sprintf("Task %v", index), url)
-	if index == 5 {
-		//test to see if you can make a object larger to fill up more space//
-		// enlarger := canvas.NewRectangle(theme.BackgroundColor())
-		enlarger := canvas.NewRectangle(testColor)
-		enlarger.SetMinSize(fyne.NewSize(size.Width*2, 1))
-		tile.Add(enlarger)
-		hyperlink.Resize(fyne.NewSize(size.Width*2, size.Height))
-	} else {
-		hyperlink.Resize(fyne.NewSize(size.Width, size.Height))
-	}
+	// enlarger := canvas.NewRectangle(theme.BackgroundColor())
+	durationLine := canvas.NewRectangle(testColor)
+	durationLine.SetMinSize(fyne.NewSize(size.Width*float32(duration), 1))
+	tile.Add(durationLine)
+
+	//url, _ := url.Parse("https://developer.fyne.io/api/v2.1/widget/hyperlink.html")
+	url, _ := url.Parse(urlString)
+	hyperlink := widget.NewHyperlink(taskTile, url)
 	tile.Add(hyperlink)
 
 	toolbar := widget.NewToolbar(
 		widget.NewToolbarSeparator(),
 		widget.NewToolbarAction(theme.DocumentCreateIcon(), func() {
-			log.Println("New document")
+			log.Printf("Edit %s\n", taskTile)
 		}),
 		//widget.NewToolbarSeparator(),
 		widget.NewToolbarAction(theme.MediaFastRewindIcon(), func() {}),
 		widget.NewToolbarAction(theme.MediaPauseIcon(), func() {
-			log.Println("tile shouldn't move now")
+			log.Printf("lock %s in time", taskTile)
 		}),
 		widget.NewToolbarAction(theme.MediaFastForwardIcon(), func() {}),
 		widget.NewToolbarSpacer(),
@@ -94,57 +80,69 @@ func getTile(index int) *fyne.Container {
 
 		widget.NewToolbarSeparator(),
 	)
-	hyperlink.Resize(fyne.NewSize(size.Width, size.Height))
 	tile.Add(toolbar)
-
-	////Try to give a line border to a container//NO lines done touch, and are squared
-	// grey := color.NRGBA{R: 128, G: 128, B: 128, A: 255}
-	// top := canvas.NewRectangle(grey)
-	// bottom := canvas.NewRectangle(grey)
-	// left := canvas.NewRectangle(grey)
-	// right := canvas.NewRectangle(grey)
-	// top.SetMinSize(fyne.NewSize(500, 5))
-	// bottom.SetMinSize(fyne.NewSize(500, 5))
-	// left.SetMinSize(fyne.NewSize(5, 500))
-	// right.SetMinSize(fyne.NewSize(5, 500))
-
-	// tileWithBorder := container.New(layout.NewBorderLayout(top, bottom, left, right),
-	// 	top, bottom, left, right, tile)
-	// return tileWithBorder
-
-	////this just gives small space between items
-	// paddedTile := container.NewPadded(tile)
-	// return paddedTile
-
-	////Try to set the size of the tile when created
-	//tile.Resize(fyne.NewSize(tile.Size().Width*2,tile.Size().Height))
-	//tile.NewSize(fyne.NewSize(100, 100))
 
 	return tile
 }
 
-func getBoardTiles() *fyne.Container {
-	var gridSize = fyne.NewSize(4, 4)
-	//board := container.New(layout.NewGridLayout(int(gridSize.Width)))
-	//board := container.New(layout.NewGridWrapLayout(fyne.NewSize(200, 100)))
-	board := container.NewHBox() //create new layout that is this, but has wrap capabilites
+// https://stackoverflow.com/questions/38775414/golang-date-time-struct
+type Tile struct {
+	tile     string
+	url      string
+	Date     time.Time
+	duration int
+}
 
-	for i := 0; i < int(gridSize.Width)*int(gridSize.Height); i++ {
-		//tile := widget.NewCard("title", fmt.Sprintf("%v", i), canvas.NewImageFromResource(theme.FyneLogo()))
-		tile := getTile(i)
+func getDaysTiles() *fyne.Container {
 
-		if i == 6 || i == 8 || i == 9 {
-			//tests if you can have empty space//yes
-			board.Add(layout.NewSpacer())
+	day := container.NewHBox() //create new layout that is this, but has wrap capabilites
+
+	//this is where you'll have to get the data for tile for the given date and time, for a person
+	taskTile := "Title"
+	url := "https://developer.fyne.io/api/v2.1/widget/hyperlink.html"
+	tileDuration := 1
+	empty := false
+
+	dateData := time.Date(2021, time.December, 4, 9, 0, 0, 0, time.UTC)
+	fmt.Printf("Go launched at %s\n", dateData.Local())
+
+	tileData := Tile{tile: taskTile, url: url, Date: dateData, duration: tileDuration}
+
+	list := list.New()
+	list.PushBack(tileData)
+
+	//run for the number of tiles for a time period, or run for a time period and get coresponding tiles
+	for i := 7; i < 20; i++ {
+
+		tile := getTile(fmt.Sprintf("%v %v", tileData.tile, i), tileData.url, tileData.duration)
+
+		log.Printf("Inedex: %v tileTime: %v ", i, tileData.Date.Hour())
+
+		//figure out if the next tile is after than current time in loop
+		if tileData.Date.Hour() < i {
+			empty = true
 		} else {
-			////moved to tile createion
-			// if i == 5 {
-			// 	//test to see if you can make a object larger to fill up more space//No the grid is fixed
-			// 	tile.Resize(fyne.NewSize(tile.MinSize().Width*2, tile.MinSize().Height))
-			// }
-			board.Add(tile)
+			empty = false
+		}
+
+		//need to figure out if there is a gap that needs to be added
+		if empty {
+			day.Add(layout.NewSpacer())
+		} else {
+			day.Add(tile)
 		}
 	}
+
+	return day
+}
+
+func getBoardTiles() *fyne.Container {
+	//var gridSize = fyne.NewSize(4, 4)
+	//board := container.New(layout.NewGridLayout(int(gridSize.Width)))
+	//board := container.New(layout.NewGridWrapLayout(fyne.NewSize(200, 100)))
+	board := container.NewVBox() //create new layout that is this, but has wrap capabilites
+
+	board.Add(getDaysTiles())
 
 	return board
 }
@@ -153,7 +151,8 @@ func getBoard() *fyne.Container {
 	//img := canvas.NewImageFromResource(theme.FyneLogo())
 	board := getBoardTiles()
 	//return container.New(layout.NewMaxLayout(), board)
-	return board
+	return container.New(layout.NewMaxLayout(), container.NewHScroll(board))
+	//return board
 
 }
 
@@ -208,4 +207,17 @@ func getDatesTable() *widget.Table {
 		})
 
 	return table
+}
+
+func getTabContent() *fyne.Container {
+
+	//replace to be inside the scroll layout
+	left := getDatesTable()
+	topTimes := getTimesTable(7, 13)
+	topSpacer := getTopSpacer(left.Size().Width, topTimes.Size().Height)
+	top := container.New(layout.NewBorderLayout(nil, nil, topSpacer, nil), topSpacer, topTimes)
+
+	board := getBoard()
+
+	return container.New(layout.NewBorderLayout(top, nil, left, nil), top, left, board)
 }
