0cc4e7442076ade86ffbb9c8db9084ae95153034
AshGale <ashgale17@yahoo.co.uk>
2021-11-08 20:43:00 +0000

    added support to read form json file

--- src/tile.go
+++ src/tile.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"image/color"
+	"io/ioutil"
 	"log"
 	"net/url"
 	"somethingFyne/util"
@@ -23,7 +24,20 @@ type Tile struct {
 	Url      string
 	Date     time.Time
 	Duration int
-	Color    color.Color
+	// https://stackoverflow.com/questions/53419447/how-to-marshal-and-unmarshal-a-color-palette-to-json-in-go/53419969
+	Color color.RGBA
+}
+
+func getDaysTileData(day int) []Tile {
+	fileName := fmt.Sprintf("./db/day%v.json", day)
+	// fmt.Printf("Reading from file %v\n", fileName)
+	file, _ := ioutil.ReadFile(fileName)
+	var tileList []Tile
+	_ = json.Unmarshal([]byte(file), &tileList)
+	//fmt.Printf("from file:\n %v\n", string(file))
+	//fmt.Printf("from []Tile:\n %v\n", tileList)
+
+	return tileList
 }
 
 func getEmptyTile(duration int) *fyne.Container {
@@ -41,7 +55,7 @@ func makeTileContainer(tile Tile) *fyne.Container {
 	tileContainer := container.New(layout.NewVBoxLayout())
 
 	// enlarger := canvas.NewRectangle(theme.BackgroundColor())
-	durationLine := canvas.NewRectangle(util.GreenColor)
+	durationLine := canvas.NewRectangle(tile.Color)
 	durationLine.SetMinSize(fyne.NewSize(util.TileSize.Width*float32(tile.Duration), util.TileLineHeight))
 	tileContainer.Add(durationLine)
 
@@ -71,46 +85,13 @@ func makeTileContainer(tile Tile) *fyne.Container {
 	return tileContainer
 }
 
-type test struct {
-	One  string
-	Two  int
-	Date time.Time
-}
-
-func getDaysTileData(day int) []Tile {
-	//this is where you'll have to get the data for tile for the given date and time, for a person
-	taskTile := "Title"
-	url := "https://developer.fyne.io/api/v2.1/widget/hyperlink.html"
-	tileDuration := 1
-	dateData := time.Date(2021, time.December, 4, 9, 0, 0, 0, time.UTC)
-	color := util.GreenColor
-
-	tile := Tile{Heading: taskTile, Url: url, Date: dateData, Duration: tileDuration, Color: color}
-
-	var tileList []Tile
-
-	tileList = append(tileList, tile)
-	tile.Duration = 2
-	tile.Date = tile.Date.Add(time.Hour * 3)
-	tileList = append(tileList, tile)
-
-	t, err := json.Marshal(tileList)
-	if err != nil {
-		fmt.Printf("Error: %s", err)
-	}
-	fmt.Println(string(t))
-
-	return tileList
-}
-
 func getDaysTiles(day int) *fyne.Container {
-
 	daysTiles := container.NewHBox() //create new layout that is this, but has wrap capabilites
 	tileList := getDaysTileData(day)
 	empty := false
 
 	//run for the number of tiles for a time period, or run for a time period and get coresponding tiles
-	for i, h := 0, util.FirstTimeShown; h < util.FirstTimeShown+util.TimesShown; h++ {
+	for i, h := 0, util.FirstTimeShown; h < util.FirstTimeShown+util.TimesShown; {
 
 		if len(tileList) == i {
 			h = 100
@@ -120,7 +101,7 @@ func getDaysTiles(day int) *fyne.Container {
 
 		//tileData := front.Value.(Tile)
 		//tileData := Tile{tile: taskTile, url: url, Date: dateData, duration: tileDuration, color: theme.BackgroundColor()}
-		tileData.Heading = fmt.Sprintf("%v %v", tileData.Heading, h)
+		//tileData.Heading = fmt.Sprintf("%v %v", tileData.Heading, h)
 		tile := makeTileContainer(tileData)
 		// tile := makeTile(fmt.Sprintf("%v %v", tileData.heading, i), tileData.url, tileData.duration)
 
@@ -135,15 +116,17 @@ func getDaysTiles(day int) *fyne.Container {
 
 		//need to figure out if there is a gap that needs to be added
 		if empty {
-			log.Printf("\tAdded empty tile")
+			log.Printf("\tEmpty tile at %v", h)
 			//daysTiles.Add(layout.NewSpacer())//only works with set grid sizes
 			daysTiles.Add(getEmptyTile(1))
+			h++
 		} else {
-			log.Printf("\tAdded new tile at time %v", tileData.Date.Hour())
+			log.Printf("\tAdd tile at time %v", h)
 			//todo, might be the place to update the hour of the tile,
 			daysTiles.Add(tile)
 			//tileList.Remove(tileList.Front())
 			i++
+			h += tileData.Duration
 		}
 		fmt.Printf("index %v, hour %v\n", i, h)
 	}
